2a79de7076b9e33fe36f3587bb3e3232c3c38968
davidmaya79 <davidmaya79@gmail.com>
2022-11-09 16:43:18 -0300

    devuelta alos commits despues de reinstalar xampp

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <title>Aprendible -{{$title ?? ''}}</title>
+    <meta name="description" content="{{ $metaDescription ??   'Default meta description'}}"/>  
+ </head>
+ <body>
+    {{-- app guarda todo y se manda a llamar con @extend ('layouts')--}}
+ 
+ <x-navigation />
+
+{{$slot}}
+  
+</body>
+   </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,8 @@
- @extends('layouts.app') {{-- directiva de blade que recive el nombre de la vista que queremos utilizar como plantilla asume que las plantillas estan en las carpetas view .layouts--}}
+{{-- @component('components.layout') --}}
+ <x-layout title="Home" meta-Description="Home meta description">
+  
 
-    @section('title', 'Home'){{-- aca no necesita @endsection porque no es codigo html--}}
-
-    @section('meta-description','Home meta description')
-       
-     
-
-@section('content')
-   <h1>Home</h1>
-    
-@endsection
+  <h1>Home</h1>    
+  
+ </x-layout>
+{{--  @endcomponent --}}
